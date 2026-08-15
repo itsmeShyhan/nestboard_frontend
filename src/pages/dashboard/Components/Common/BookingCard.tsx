0c97ec52895e3ca9f3ebe4d3349a6ab49fcea984
itsmeshyhan <shyhanwahab@gmail.com>
2026-08-15 10:50:25 +0530

    Added view button UI for current booking and favorite properties part of dashboard with hardcoded data

--- src/pages/dashboard/Components/Common/BookingCard.tsx
+++ src/pages/dashboard/Components/Common/BookingCard.tsx
@@ -1,4 +1,5 @@
 
+import { Button } from '@/components/ui/button'
 import type { Property } from '@/types/property'
 import { Calendar, MapPin } from 'lucide-react'
 
@@ -8,35 +9,47 @@ const BookingCard = (property: Property) => {
   console.log(property)
   return (
     <>
-    <div className='flex flex-row text-left gap-5 p-8 shadow-xl'>
+    <div className='flex flex-row  p-8 shadow-xl justify-between'>
       
-      {/* Property Image */}
-      <div>
-        <img className='size-30 rounded-2xl object-cover' src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=1200&h=500&fit=crop" alt="" />
-      </div>
+      {/* Property Image and name, room name, location and date of lease */}
+      <div className='flex flex-row text-left gap-5'>
 
-      <div className='flex flex-col gap-7'>
-
-        {/* Property Name */}
-        <h1 className='font-bold'>Sunset Apartment</h1>
-
-        {/* Room Number */}
-        <caption className='font-light text-left'>Room A</caption>
-        
-        {/* location and Date */}
-        <div className='flex flex-row font-light gap-3'>
-
-          <div className='flex flex-row'>
-            <MapPin strokeWidth={1}></MapPin>
-            <p className='relative top-0.5'>Colombo</p>
-          </div>
-
-          <div className='flex flex-row font-light'>
-            <Calendar strokeWidth={1}></Calendar>
-            <p className='relative top-0.5'>2024-01-15 to 2024-04-15</p>
-          </div>
+         {/* Property Image */}
+        <div>
+          <img className='size-30 rounded-2xl object-cover' src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=1200&h=500&fit=crop" alt="" />
         </div>
+
+        {/* property name, room name, location and date of lease */}
+        <div className='flex flex-col gap-7'>
+
+          {/* Property Name */}
+          <h1 className='font-bold'>Sunset Apartment</h1>
+
+          {/* Room Number */}
+          <caption className='font-light text-left'>Room A</caption>
+          
+          {/* location and Date */}
+          <div className='flex flex-row font-light gap-3'>
+
+            <div className='flex flex-row'>
+              <MapPin strokeWidth={1}></MapPin>
+              <p className='relative top-0.5'>Colombo</p>
+            </div>
+
+            <div className='flex flex-row font-light'>
+              <Calendar strokeWidth={1}></Calendar>
+              <p className='relative top-0.5'>2024-01-15 to 2024-04-15</p>
+            </div>
+          </div>
+
+          <Button className='w-40'>View Property</Button>
+        </div>
+
+        
+
       </div>
+
+      
     </div>
     </>
   )
--- src/pages/dashboard/Components/Common/FavoritesCard.tsx
+++ src/pages/dashboard/Components/Common/FavoritesCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import type { Property } from '@/types/property'
 import { MapPin } from 'lucide-react'
 
@@ -9,34 +10,52 @@ const FavoritesCard = (property: Property) => {
   return (
     <>
     
-    
+    <div>
 
-    <div className='flex flex-row text-left gap-5 p-8 shadow-xl'>
-      
-      {/* Property Image */}
-      <div>
-        <img className='size-30 rounded-2xl object-cover' src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=1200&h=500&fit=crop" alt="" />
-      </div>
+         {/* Property Image and details */}
+    <div className='flex flex-row text-left gap-5 p-8 shadow-xl justify-between'>
 
-      <div className='flex flex-col gap-7'>
-
-        {/* Property Name */}
-        <h1 className='font-bold'>Green Villa Colombo</h1>
-
-        {/* Location */}
-
-        <div className='flex flex-row'>
-            <MapPin strokeWidth={1}></MapPin>
-            <p className='relative top-0.5'>Ethul Kotte</p>
-        </div>
+      <div className='flex flex-row text-left gap-5 '>
         
-        {/* Pricing Per Moth */}
-        <div className='flex flex-row font-light gap-3'>
-          <p className='text-primary font-extrabold'>LKR 20,000-30,000  </p>
-          <p>/month</p>
+           {/* Property Image */}
+        <div>
+          <img className='size-30 rounded-2xl object-cover' src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=1200&h=500&fit=crop" alt="" />
         </div>
+
+
+        {/* Name, location and price of property */}
+        <div className='flex flex-col gap-7'>
+
+          {/* Property Name */}
+          <h1 className='font-bold'>Green Villa Colombo</h1>
+
+          {/* Location */}
+
+          <div className='flex flex-row'>
+              <MapPin strokeWidth={1}></MapPin>
+              <p className='relative top-0.5'>Ethul Kotte</p>
+          </div>
+          
+          {/* Pricing Per Moth */}
+          <div className='flex flex-row font-light gap-3'>
+            <p className='text-primary font-extrabold'>LKR 20,000-30,000  </p>
+            <p>/month</p>
+          </div>
+        </div>
+
       </div>
+      
+   
+
+       <Button className='relative top-22'>View</Button>
     </div>
+
+   
+
+    </div>
+
+ 
+    
     </>
   )
 }
