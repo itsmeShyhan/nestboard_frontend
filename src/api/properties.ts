e4b7ba791c3afb77f583a886a75a3a3ad70c0c24
itsmeshyhan <shyhanwahab@gmail.com>
2026-08-17 10:59:45 +0530

    Changed the output of the fetch properties to fit the data sent by the backend API

--- src/api/properties.ts
+++ src/api/properties.ts
@@ -1,12 +1,17 @@
 import type { Property, PropertyDetail } from "@/types/property"
+import { API_URL } from "./config"
+
+
 
 export async function fetchProperties(): Promise<Property[]> {
-  const res = await fetch("http://localhost:3001/api/properties")
+  const res = await fetch(`${API_URL}/properties`)
   if (!res.ok) throw new Error("Failed to fetch properties")
-  return res.json()
+  const body = await res.json();
+  return Array.isArray(body) ? body : body.data
+  // return res.json()
 }
 export async function fetchPropertyDetail(id: string): Promise<PropertyDetail> {
-  const res = await fetch(`http://localhost:3001/api/properties/${id}`)
+  const res = await fetch(`${API_URL}/properties/${id}`)
   if (!res.ok) throw new Error(`Failed to fetch property: ${id}`)
   return res.json()
 }
